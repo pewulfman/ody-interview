001ecdda5ff53f3c02cc27ff762d979a77f3b654
Pierre-Emmanuel Wulfman <pierre.emmanuel.wulfman@gmail.com>
2024-01-07 23:45:47 +0100

    add basic security setup

---
+++ src/common/errors.ts
@@ -0,0 +1,1 @@
+export const E_TOO_MANY_REQUESTS = 'Too many requests';
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,35 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
+import { E_TOO_MANY_REQUESTS } from './common/errors';
 
 async function bootstrap() {
+  // - NestJS app setup
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // - Basic security setup
+  // -- Helmet setup (https://helmetjs.github.io/)
+  app.use(helmet());
+
+  // -- Cors setup
+  app.enableCors({
+    origin: false,
+  });
+
+  // -- Rate limiting: Limits the number of requests from the same IP in a period of time.
+  app.use(
+    rateLimit({
+      windowMs: 10 * 60 * 1000, // in milliseconds
+      max: 100,
+      standardHeaders: true,
+      legacyHeaders: false,
+      skipSuccessfulRequests: false,
+      message: { message: E_TOO_MANY_REQUESTS, statusCode: 403 },
+    }),
+  );
+
+  // - Start app
+  await app.listen(process.env.PORT ? parseInt(process.env.PORT) : 3000);
 }
 bootstrap();
