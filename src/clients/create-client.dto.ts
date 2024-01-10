060fa87a37372f48d13998483d771113a6916f7e
Pierre-Emmanuel Wulfman <pierre.emmanuel.wulfman@gmail.com>
2024-01-10 15:41:19 +0100

    fix api doc

--- src/clients/create-client.dto.ts
+++ src/clients/create-client.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IsDate,
   IsDefined,
@@ -9,30 +10,36 @@ export class CreateClientDto {
   @IsDefined()
   @IsEmail()
   @IsNotEmpty()
+  @ApiProperty()
   readonly email: string;
 
   @IsDefined()
   @IsString()
   @IsNotEmpty()
+  @ApiProperty()
   readonly language: string;
 
   @IsDefined()
   @IsString()
   @IsNotEmpty()
+  @ApiProperty()
   readonly countryOfOrigin: string;
 
   @IsDefined()
   @IsString()
   @IsNotEmpty()
+  @ApiProperty()
   readonly countryOfDestination: string;
 
   @IsDefined()
   @IsDate()
   @IsNotEmpty()
+  @ApiProperty()
   readonly travelDateStart: Date;
 
   @IsDefined()
   @IsDate()
   @IsNotEmpty()
+  @ApiProperty()
   readonly travelDateEnd: Date;
 }
